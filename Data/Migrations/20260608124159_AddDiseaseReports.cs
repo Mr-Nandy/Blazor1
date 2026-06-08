73df548a04caff4810374adff2b11aa3d06cf510
Mr-Nandy <2021mr.nandy@gmail.com>
2026-06-08 15:44:00 +0300

    Add disease reports migration

---
+++ Data/Migrations/20260608124159_AddDiseaseReports.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blazor1.Data.Migrations
+{
+    public partial class AddDiseaseReports : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DiseaseReports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    District = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReportDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DiseaseName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AnimalType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SickCount = table.Column<int>(type: "int", nullable: false),
+                    DeadCount = table.Column<int>(type: "int", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ResponsiblePerson = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiseaseReports", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiseaseReports");
+        }
+    }
+}
